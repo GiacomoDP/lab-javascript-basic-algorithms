5241149e94dea81147cf022703d4d9793eb9a322
Giacomo De Pol <giacomo.depol@hotmail.it>
2022-05-02 16:13:20 +0200

    1.4 lab

--- js/index.js
+++ js/index.js
@@ -8,7 +8,8 @@ let hacker1 = "Giacomo"
 console.log `"The driver's name is ${hacker1}"`
 
 //1.3 Create a variable `hacker2` with the navigator's name.
-
+let hacker2 = "Luca"
 //1.4 Print `"The navigator's name is YYYY"`. //
+console.log `"The navigator's name is ${hacker2}"`
 
 // Iteration 3: Loops
