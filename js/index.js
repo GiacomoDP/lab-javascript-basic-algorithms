7b9195392031e519553be1339c8e0415c418d690
Giacomo De Pol <giacomo.depol@hotmail.it>
2022-05-02 17:11:23 +0200

    lab 2.0

--- js/index.js
+++ js/index.js
@@ -5,11 +5,20 @@
 let hacker1 = "Giacomo"
 
 //1.2 Print `"The driver's name is XXXX"`.
-console.log `"The driver's name is ${hacker1}"`
-
+console.log (`"The driver's name is ${hacker1}"`)
 //1.3 Create a variable `hacker2` with the navigator's name.
 let hacker2 = "Luca"
 //1.4 Print `"The navigator's name is YYYY"`. //
-console.log `"The navigator's name is ${hacker2}"`
+console.log (`"The navigator's name is ${hacker2}"`)
+
+if (hacker1.length>hacker2.length ) {
+        console.log (`The driver has the longest name, it has ${hacker1} characters.`);
+} else if (hacker2.lenght>hacker1.lenght)  {
+    console.log (`It seems that the navigator has the longest name, it has ${hacker2} characters.`);
+  } else {
+    console.log(`Wow, you both have equally long names, XX characters!`);
+  }
+  ;
+
 
 // Iteration 3: Loops
